c2f09ee89f8d30f527a77ba0e485280415ec17fc
Meg Schneider <megschneider616@gmail.com>
2023-03-01 17:00:22 -0500

    github and linkedin links for each person below photos

--- pages/about.tsx
+++ pages/about.tsx
@@ -4,6 +4,7 @@ import { Inter } from 'next/font/google'
 import styles from '@/styles/Home.module.css'
 import React from "react";
 import { Grid, Typography } from "@material-ui/core";
+import Link from '@material-ui/core/Link';
 import { makeStyles } from "@material-ui/core/styles";
 
 const inter = Inter({ subsets: ['latin'] })
@@ -23,45 +24,43 @@ const useStyles = makeStyles((theme) => ({
 const teamMembers = [
   {
     name: "Melody Duan",
+    github: "https://github.com/MelodyDuan",
+    linkedIn: "https://www.linkedin.com/in/melodyduan",
     imageSrc: "melody-circle.png",
   },
   {
     name: "Morry Kolman",
+    github: "https://github.com/wttdotm",
+    linkedIn: "https://www.linkedin.com/in/morry-kolman-66072299/",
     imageSrc: "morry-circle.png",
   },
   {
     name: "Meg Schneider",
+    github: "https://github.com/meg-schneider",
+    linkedIn: "https://www.linkedin.com/in/schneidermeg/",
     imageSrc: "meg-circle.png",
   },
   {
     name: "Justin Ribarich",
+    github: "https://github.com/JustinRibs",
+    linkedIn: "https://www.linkedin.com/in/justin-ribarich/",
     imageSrc: "justin-circle.png",
   },
   {
     name: "Jordan Cepeda",
+    github: "https://github.com/JordanMcLuvin",
+    linkedIn: "https://www.linkedin.com/in/jordancepeda/",
     imageSrc: "jordan-circle.png",
   },
 ];
 
-// export default function Home() {
-//   return (
-//     <main className={styles.main}>
-//       <div className={styles.info}>
-//         <p><i style={{ color: '#05C7C7' }}>
-//           About the Team
-//         </i>
-//         </p>
-//       </div>
-//     </main>
-//   )
-// }
 
 function AboutPage() {
   const classes = useStyles();
 
   return (
     <div className={styles.main}>
-      <Typography variant="h3" gutterBottom>
+      <Typography variant="h3" style={{marginBottom: "50px"}}>
         About the Team
       </Typography>
       <Grid container spacing={4}>
@@ -70,8 +69,15 @@ function AboutPage() {
             <div style={{ display: "flex", alignItems: "center", justifyContent: "center" }}>
             <img src={member.imageSrc} alt={member.name} className={classes.image} />
             </div>
-            <div style={{textAlign: "center" }}>
+            <div style={{textAlign: "center", marginTop: "15px"}}>
             <Typography variant="h5">{member.name}</Typography>
+            <Link href={member.github}>
+            Github
+            </Link>
+            <br></br>
+            <Link href={member.github}>
+            LinkedIn
+            </Link>
             <Typography>Github</Typography>
             <Typography>LinkedIn</Typography>
             </div>
